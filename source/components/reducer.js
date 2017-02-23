01ef44babbda9766e230fb959175d84063a222a5
Daniel Shaefer <daniel@dwolla.com>
2017-02-23 17:20:42 -0600

    Update reducer to handler the storage of the checkboxes that are currently on

--- source/components/reducer.js
+++ source/components/reducer.js
@@ -3,6 +3,18 @@ const reducer = (state, action) => {
     switch (action.type) {
         case Actions.TREASURE_VALUE_CHANGED:
             return { ...state, treasureValue: action.value };
+        case Actions.TREASURE_TYPE_CHANGED: {
+            const checked = action.checked
+            const treasureType = action.treasureType
+            const newTreasureTypesOn = [...state.treasureTypesOn]
+            const indexOfTreasureType = state.treasureTypesOn.indexOf(treasureType)
+            if (checked && indexOfTreasureType == -1)
+                newTreasureTypesOn.push(treasureType)
+            if (!checked && indexOfTreasureType != -1)
+                newTreasureTypesOn.splice(indexOfTreasureType, 1)
+            console.warn(newTreasureTypesOn)
+            return { ...state, treasureTypesOn:newTreasureTypesOn}
+        }
         default:
             return state;
     }
